1a1b3f926a4be8b659b987648169a4db19a5f415
Ilya Sher <34807039+ilyash-b@users.noreply.github.com>
2024-03-22 19:30:44 +0400

    Cosmetics - types

--- lambda/src/index.ts
+++ lambda/src/index.ts
@@ -8,7 +8,7 @@ const debug = DEBUG('beame:lambda:index');
 const app = initApp();
 
 export const handler = serverless(app, {
-	request(request: RequestWithContext, event: {requestContext: ContextType}, context: Object) {
+	request(request: RequestWithContext, event: {requestContext: ContextType}, context: object) {
 		debug('event: %o', event);
 		debug('context: %o', context);
 		request.context = event.requestContext; // allow requestContext to be accessed in routes (used for iam auth)
